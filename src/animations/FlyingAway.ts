cafd7dd793793d42169547ad04d92a5b72b995c3
aleksandra <sasha.pleshyova@yandex.ru>
2022-05-16 22:41:49 +0300

    Add FlyingAway animation

--- src/animations/FlyingAway.ts
+++ src/animations/FlyingAway.ts
@@ -1,8 +1,8 @@
 import {FlyingAwayOptionsType} from "../types";
 
 const FLYING_AWAY__OPTIONS = {
-  maxOffsetX: 10,
-  maxOffsetY: 10,
+  maxOffsetX: 100,
+  maxOffsetY: 100,
   transition: '.4s',
 }
 
@@ -17,21 +17,26 @@ export class FlyingAway {
     this.element = element;
 
     this.element.style.transform = 'translate(0px, 0px)';
-    this.element.style.transition = `${this.options.transition} ease-out`;
+    this.element.style.transition = `transform ${this.options.transition} ease-out`;
     document.documentElement.addEventListener('mousemove', this.move);
 
     const targetCoords = this.element.getBoundingClientRect();
     this.initialCenter = {
-      x: targetCoords.left - targetCoords.width / 2,
-      y: targetCoords.top - targetCoords.height / 2,
+      x: targetCoords.left + targetCoords.width / 2,
+      y: targetCoords.top + targetCoords.height / 2,
     }
   }
 
   move = (e: MouseEvent) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
-    const xTransform = this.options.maxOffsetX - (mouseX - this.initialCenter.x);
-    const yTransform = this.options.maxOffsetY - (mouseY - this.initialCenter.y);
+
+    const offsetX = - mouseX + this.initialCenter.x;
+    const offsetY = - mouseY + this.initialCenter.y;
+
+    const xTransform = offsetX/Math.abs(offsetX) * Math.min(Math.abs(offsetX), this.options.maxOffsetX);
+    const yTransform = offsetY/Math.abs(offsetY) * Math.min(Math.abs(offsetY), this.options.maxOffsetY);
+
     this.element.style.transform = `translate(${xTransform}px, ${yTransform}px)`;
   }
 }
